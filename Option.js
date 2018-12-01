48251e976ddb1bf6568a9b55c8ea38a419775a63
Sach <jayaweerasachin@gmail.com>
2018-12-01 17:40:28 -0500

    ppppppppppeeeeeeeeennnnniiiiisss

---
+++ Option.js
@@ -0,0 +1,6 @@
+class Option{
+  constructor(name, available){
+    this.name = name;
+    this.available = available;
+  }
+}
--- index.js
+++ index.js
@@ -1,3 +1,48 @@
-/*
-*   
-*/
+let orderedPeople = []; //final variable of ordered people
+let people; //non ordered people
+let heightestValue;
+
+let numOfPeople = 5;
+
+orderPeople();
+
+
+function setAllRoles(){
+  for(var i = 0; i < player; i++){
+
+  }
+}
+
+function getRoll(){
+  return Math.floor(Math.random() * 20);
+}
+
+function orderPeople(){
+  let tempHighestValue; //holder for the highest value
+
+  for(let i = 0; i < numOfPeople; i++){ //Picks are largest, then second largest, then etc
+    tempHighestValue = 0; //highest value in the remaining set
+
+    for(let j = 0; j < numOfpeople; j++){ //loops through remaining set
+      if(people[heightestValue].roll > tempHighestValue){ //finds largest value in remaining set
+        tempHighestValue = people[j].roll;
+        heightestValue = j; // heightValue = person with the highest value in the remaining set
+      }
+    }
+
+    orderedPeople[i] = people[heightestValue].roll; //sets highest person in the remaining set and orders them
+    people[heightestValue].roll = -1; //removes highest person from the remaining set
+  }
+}
+
+function assignOption(){
+  for(var i = 0; i < people.length; i++){
+    for(var j = 0; j < options.length; j++){
+      if(option[j].available == true){
+        orderedPeople[i].result = option[j].name;
+        option[j].available = false;
+        break;
+      }
+    }
+  }
+}
